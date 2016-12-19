023e875df5440e7ddf737afc4528646ee04a9196
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2016-12-19 15:05:13 -0500

    finished get requests

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var path = require('path')
 var db = require('./models') 
 
 const Post = require('./models/index.js').Post
+const Comment = require('./models/index.js').Comment
+const Vote = require('./models/index.js').Vote
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json())
@@ -15,6 +17,33 @@ app.get('/api/post',(req,res)=>{
 	Post.findAll()
 	.then((data)=>{
 		console.log(data, 'We got all the post')
+		res.send(data);
+	}).catch((error)=>{
+		res.send(error)
+	})
+}) 
+app.get('/api/post',(req,res)=>{
+	Post.findAll()
+	.then((data)=>{
+		console.log(data, 'We got all the post')
+		res.send(data);
+	}).catch((error)=>{
+		res.send(error)
+	})
+}) 
+app.get('/api/comment',(req,res)=>{
+	Comment.findAll()
+	.then((data)=>{
+		console.log(data, 'We got all the comments')
+		res.send(data);
+	}).catch((error)=>{
+		res.send(error)
+	})
+})
+app.get('/api/vote',(req,res)=>{
+	Vote.findAll()
+	.then((data)=>{
+		console.log(data, 'We got all the votes')
 		res.send(data);
 	}).catch((error)=>{
 		res.send(error)
